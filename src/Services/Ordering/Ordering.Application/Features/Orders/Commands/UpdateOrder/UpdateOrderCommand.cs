193a2126e4d607aea42b852d223ff2ce045f2ba0
Francois-Labarre <f_labarre@hotmail.com>
2021-10-27 06:16:22 -0700

    Features

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Ordering.Domain.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommand : IRequest
+    { 
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        //billing address
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        //payment
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string cvv { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
